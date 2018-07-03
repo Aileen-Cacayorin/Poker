294584a091a6fbfaf00abf4bc0b93d891ebc78c8
Aileen Cacayorin <aileen.cacayorin@gmail.com>
2018-07-03 14:28:45 -0700

    refactor

--- Poker/main.cpp
+++ Poker/main.cpp
@@ -15,8 +15,26 @@
 #include <iomanip>
 
 
-
 using namespace std;
+
+void printHeader() {
+    cout << "Trial  ";
+    cout << "# of Hands ";
+    cout << "# of Flushes   ";
+    cout << "# of Pairs     ";
+    cout << "Flush %     ";
+    cout << "Pair % " <<endl;
+};
+
+void runTrial(int n) {
+    for (int i=0; i < n; i++) {
+        Game poker(10000);
+        for (int i=0; i<poker.totalHands; i++) {
+            poker.playHand();
+        }
+        cout << "\t" << i+1 <<"\t\t"<< poker.totalHands << "\t\t" << poker.getTotalFlushes() << "\t\t\t" << poker.getTotalPairs() <<"\t\t" <<  poker.calculateFlushPercentage() << "%\t\t" <<  poker.calculatePairPercentage() << "%" << endl;
+    }
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -25,25 +43,8 @@ int main(int argc, const char * argv[]) {
     
     int trials = 10;
     cout << fixed << setprecision(2);
-    cout << "Trial  ";
-    cout << "# of Hands ";
-    cout << "# of Flushes   ";
-    cout << "# of Pairs     ";
-    cout << "Flush %     ";
-    cout << "Pair % " <<endl;
-    for (int i=0; i < trials; i++) {
-    
-        Game poker(10000);
-        for (int i=0; i<poker.totalHands; i++) {
-            poker.playHand();
-        }
-        
-        cout << "\t" << i+1 <<"\t\t"<< poker.totalHands << "\t\t" << poker.getTotalFlushes() << "\t\t\t" << poker.getTotalPairs() <<"\t\t" <<  poker.calculateFlushPercentage() << "%\t\t" <<  poker.calculatePairPercentage() << "%" << endl;
-        
-    }
-
-   
-   
+    printHeader();
+    runTrial(trials);
     
     return 0;
 };
